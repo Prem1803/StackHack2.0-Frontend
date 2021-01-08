2ec6a08b28ed32506abe218e3c19dc668161358d
Navneet Singh <53988436+Navneet-Singh-123@users.noreply.github.com>
2021-01-08 14:37:36 +0530

    Completed slider for homepage

---
+++ src/Components/Navneet/Home.js
@@ -0,0 +1,11 @@
+import Layout from './Layout'
+
+const Home = () =>{
+    return (
+        <Layout title="Home Page" description="Home page des" className="">
+            <div>Home page</div>
+        </Layout>
+    )
+}
+
+export default Home;
---
+++ src/Components/Navneet/Slider.js
@@ -0,0 +1,49 @@
+import React from "react";
+import Slider from "react-slick";
+import img1 from '../../Resources/images/Slider/img1.jpg'
+import img2 from '../../Resources/images/Slider/img2.jpg'
+import img3 from '../../Resources/images/Slider/img3.jpg'
+
+const SlidingImage = () => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    speed: 500,
+    dots: true 
+  };
+
+  return (
+    <div
+      className=""
+      style={{ overflow: "hidden", height: "400px", background: "red" }}
+    >
+      <Slider {...settings}>
+        <div>
+          <div
+            className=""
+            style={{
+              background: `url(${img1}) no-repeat`,
+              height: "400px",
+              backgroundSize: "100% 100%"
+            }}
+          ></div>
+        </div>
+        <div>
+          <div
+            className=""
+            style={{ background: `url(${img2}) no-repeat`, height: "400px", backgroundSize: "100% 100%"}}
+          ></div>
+        </div>
+        <div>
+          <div
+            className=""
+            style={{ background: `url(${img3}) no-repeat`, height: "400px", backgroundSize: "100% 100%" }}
+          ></div>
+        </div>
+      </Slider>
+    </div>
+  );
+};
+
+export default SlidingImage;
