1abe2b880684032a407b4bb4842b705df32919d1
Navneet Singh <53988436+Navneet-Singh-123@users.noreply.github.com>
2021-01-09 11:46:38 +0530

    Navbar modifications

--- src/Components/Navneet/Navbar.js
+++ src/Components/Navneet/Navbar.js
@@ -1,7 +1,19 @@
+import {Link} from 'react-router-dom'
+
 const Navbar = () =>{
      return (
         <div className="nav_header">
-            Navbar
+            <div className="nav_container">
+                <div className="brand">
+                    <Link to="/"><i class="fas fa-cookie-bite"></i></Link>
+                </div>
+                <nav>
+                    <ul>
+                        <li><Link to="/login" className="nav_link" style={{ textDecoration: 'none' }}>Login</Link></li>
+                        <li><Link to="/signup" className="nav_link" style={{ textDecoration: 'none' }}>Sign Up</Link></li>
+                    </ul>
+                </nav>
+            </div>
         </div>        
      )
 }
